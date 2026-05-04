108858686746bc27b2d6134074a39cd4ea32e653
Ilyes512 <ilyes.ahidar@gmail.com>
2026-05-04 13:44:40 +0200

    Extended the template validate command

--- pkg/cmd/template_validate.go
+++ pkg/cmd/template_validate.go
@@ -1,16 +1,21 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/specsnl/specs-cli/pkg/specs"
+	pkgtemplate "github.com/specsnl/specs-cli/pkg/template"
+	"github.com/specsnl/specs-cli/pkg/util/exit"
 )
 
 func newTemplateValidateCmd(app *App) *cobra.Command {
-	return &cobra.Command{
+	var strict bool
+
+	cmd := &cobra.Command{
 		Use:   "validate <path>",
 		Short: "Validate a template directory",
 		Args:  cobra.ExactArgs(1),
@@ -37,8 +42,40 @@ func newTemplateValidateCmd(app *App) *cobra.Command {
 				return fmt.Errorf("template render error: %w", err)
 			}
 
-			app.Output.Info("template is valid")
+			issues, err := tmpl.Validate()
+			if err != nil {
+				return fmt.Errorf("validation error: %w", err)
+			}
+
+			for _, iss := range issues {
+				switch {
+				case errors.Is(iss, pkgtemplate.ErrUnknownVariable):
+					app.Output.Warn("variable %q used in %s is not defined in project.yaml", iss.Name, iss.File)
+				case errors.Is(iss, pkgtemplate.ErrUnusedVariable):
+					app.Output.Warn("variable %q is defined but never used in any template file", iss.Name)
+				case errors.Is(iss, pkgtemplate.ErrUnusedComputed):
+					app.Output.Warn("computed value %q is defined but never used in any template file", iss.Name)
+				}
+			}
+
+			code := 0
+			if pkgtemplate.HasUnknown(issues) {
+				code |= exit.ValidateUnknown
+			}
+			if strict && pkgtemplate.HasUnused(issues) {
+				code |= exit.ValidateUnused
+			}
+			if code != 0 {
+				return &exit.ExitError{Code: code}
+			}
+
+			if len(issues) == 0 {
+				app.Output.Info("template is valid")
+			}
 			return nil
 		},
 	}
+
+	cmd.Flags().BoolVar(&strict, "strict", false, "Treat unused variables and computed values as errors")
+	return cmd
 }
